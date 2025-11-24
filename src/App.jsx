6e0db7c22259517ff3826c7b333ed0c43a0e34eb
Rafael0896 <realvarez54@misena.edu.co>
2025-11-24 16:19:06 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
+export default function App() {
+    return (
+        <div className="app" role={application}>
+            <div>
+                <main>
+
+                </main>
+            </div>
+        </div>
+    );
+}
